cd87698f373b342225f7276187f616f0c3673228
Jota <jjunior@protonmail.ch>
2021-09-09 15:40:41 -0300

    converte para stateful widget

--- vai_de_ecommerce/lib/wCardproduct.dart
+++ vai_de_ecommerce/lib/wCardproduct.dart
@@ -7,85 +7,114 @@ Widget wCardProduct(List<Product> products, BuildContext context) {
   return Container(
     height: 350,
     width: 500,
-    child: ListView(
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal, children: [
-      wCard(products[0], context),
-      wCard(products[1], context),
-      wCard(products[2], context),
-      wCard(products[3], context),
-    ]
-    ),
+    child:
+        ListView(shrinkWrap: true, scrollDirection: Axis.horizontal, children: [
+      WCard(products[0]),
+      WCard(products[1]),
+      WCard(products[2]),
+      WCard(products[3]),
+    ]),
   );
 }
 
-Card wCard(Product p, BuildContext context) {
-   IconData favorite;
-  if(p.favorite){
-    favorite = Icons.favorite;
-  }else{
-    favorite = Icons.favorite_border;
-  }
-  return Card(
-    color: Colors.white70,
-    shadowColor: Colors.black26,
-    child: Stack(
-      alignment: AlignmentDirectional.topCenter,
-      children: [
-        Positioned(
-          right: 5,
-          top: 10,
-          child: TextButton(
-            child: Icon(
-              favorite,
-              color: Colors.purple,
-            ),
-            onPressed: () {
-              p.favorite = true;
-            },
-          ),
-        ),
-        Column(//coluna geral
+class WCard extends StatefulWidget {
+  final Product p;
+
+  WCard(this.p);
+
+  @override
+  _WCardState createState() => _WCardState(p);
+}
+
+class _WCardState extends State<WCard> {
+  bool isFavorite = false;
+  Product p;
+
+  _WCardState(this.p);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        color: Colors.white70,
+        shadowColor: Colors.black26,
+        child: Stack(
+          alignment: AlignmentDirectional.topCenter,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                width: 150,
-                height: 150,
-                child: Image.asset(
-                  p.url,
-                  fit: BoxFit.contain,
+            Positioned(
+              right: 5,
+              top: 10,
+              child: TextButton(
+                child: Icon(
+                  isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: Colors.purple,
                 ),
+                onPressed: () {
+                  setState(() {
+                    p.favorite = !p.favorite;
+                    isFavorite = !isFavorite;
+                  });
+                },
               ),
             ),
-            Column(//coluna interna
+            Column(
+              //coluna geral
               children: [
                 Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    child: wText(text: p.title, color: Colors.blueGrey, fontHeight: 20)
+                  padding: const EdgeInsets.all(15.0),
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    child: Image.asset(
+                      p.url,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ),
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    child: wText(text: p.desc, color: Colors.blueGrey, fontHeight: 16)),
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    child: wText(text: "R\$ ${p.value}", color: Colors.blueGrey, fontHeight: 20)),
-                wButton(width: 200,text: "Ver Mais", p: p, onTap: (){
-                  Navigator.pushNamed(context, '/product', arguments: p);
-                }, color: Colors.teal,),
+                Column(
+                  //coluna interna
+                  children: [
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        child: wText(
+                            text: p.title,
+                            color: Colors.blueGrey,
+                            fontHeight: 20)),
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        child: wText(
+                            text: p.desc,
+                            color: Colors.blueGrey,
+                            fontHeight: 16)),
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        child: wText(
+                            text: "R\$ ${p.value}",
+                            color: Colors.blueGrey,
+                            fontHeight: 20)),
+                    wButton(
+                      width: 200,
+                      text: "Ver Mais",
+                      p: p,
+                      onTap: () {
+                        Navigator.pushNamed(context, '/product', arguments: p);
+                      },
+                      color: Colors.teal,
+                    ),
 
-                /*ElevatedButton(
+                    /*ElevatedButton(
                     onPressed: (){
                       Navigator.pushNamed(context, "/product", arguments: p);
                     },
                     child: Text("Ver mais")
                 )*/
+                  ],
+                ),
               ],
             ),
           ],
-        ),
-      ],
-    ),
-  );
-
+        ));
+  }
 }
